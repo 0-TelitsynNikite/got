371396211a9b7a277ec362b97cc2b430dce5d8a3
Nikita Telitsyn <dmtvn.labs@yandex.ru>
2021-07-26 09:18:12 +0300

    GotService was moved to the separate directory

--- src/services/GotService.js
+++ src/services/GotService.js
@@ -1,6 +1,10 @@
 class GotService {
+    constructor() {
+        this._apiBase = 'https://www.anapioficeandfire.com/api'
+    }
+
     async getResource(url) {
-        const res = await fetch(url);
+        const res = await fetch(`${this._apiBase}${url}`);
 
         if (!res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
@@ -10,7 +14,11 @@ class GotService {
     }
 
     getAllCharacters() {
-        return this.getResource('https://www.anapioficeandfire.com/api/characters?page=5&pageSize=10');
+        return this.getResource(`/characters?page=5&pageSize=10`);
+    }
+
+    getCharacter(id) {
+        return this.getResource(`/characters/${id}`);
     }
 }
 
