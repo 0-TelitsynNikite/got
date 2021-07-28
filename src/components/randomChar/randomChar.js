b63158da8b11da35137076c2d0b24a4c4fad19a1
Nikita Telitsyn <dmtvn.labs@yandex.ru>
2021-07-28 10:46:25 +0300

    Added life cycle and update timer for characters

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,63 +1,94 @@
 import React, {Component} from 'react';
 import './randomChar.css';
 import GotService from "../../services/GotService";
+import Spinner from '../spinner';
+import ErrorMessage from '../errorMessage';
 
 export default class RandomChar extends Component {
 
     constructor() {
         super();
-        this.updateChar();
-    }
-    gotService = new GotService();
-    state = {
-        name: null,
-        gender: null,
-        born: null,
-        died: null,
-        culture: null
+        
     }
 
-    updateChar() {
+    gotService = new GotService();
+    
+    state = {
+        char: {},
+        loading: true,
+        error: false
+    }
+
+    onCharLoaded = (char) => {
+        this.setState({
+            char,
+            loading: false
+        })
+    }
+
+    onError = (err) => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
+
+    updateChar = (char) => {
         const id = Math.floor(Math.random() * 140 + 25);
         this.gotService.getCharacter(id)
-            .then(char => {
-                this.setState(
-                    {
-                        name: char.name,
-                        gender: char.gender,
-                        born: char.born,
-                        died: char.died,
-                        culture: char.culture
-                    }
-                )
-            })
+            .then(this.onCharLoaded)
+            .catch(this.onError);
+    }
+
+    componentDidMount() {
+        this.updateChar();
+        this.timerId = setInterval(this.updateChar, 5000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId)
     }
 
     render() {
-        const {name, gender, born, died, culture} = this.state;
+        const { char, loading, error } = this.state;
+
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || errorMessage) ? <View char={char}/> : null;
 
         return (
             <div className="random-block rounded">
-                <h4>Random Character: {name}</h4>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">{gender} </span>
-                        <span>male</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">{born} </span>
-                        <span>11.03.1039</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">{died} </span>
-                        <span>13.09.1089</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">{culture} </span>
-                        <span>Anarchy</span>
-                    </li>
-                </ul>
+                {errorMessage}
+                {spinner}
+                {content}
             </div>
         );
     }
+}
+
+const View = ({char}) => {
+    const {name, gender, born, died, culture} = char;
+    return (
+        <>
+            <h4>Random Character: {name}</h4>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">{gender} </span>
+                    <span>male</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">{born} </span>
+                    <span>11.03.1039</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">{died} </span>
+                    <span>13.09.1089</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">{culture} </span>
+                    <span>Anarchy</span>
+                </li>
+            </ul>
+        </>
+    )
 }
