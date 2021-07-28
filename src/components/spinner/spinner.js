48d460e5a3486eb0e355a15184d5f5202c5f632f
Nikita Telitsyn <dmtvn.labs@yandex.ru>
2021-07-28 10:39:54 +0300

    Added a spinner from https://loading.io/

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <>
+            <div class="loadingio-spinner-spin-3xezquf3v3v center"><div class="ldio-ac93ihzvpmf">
+            <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+            </div></div>
+        </>
+    )
+}
+
+export default Spinner;
