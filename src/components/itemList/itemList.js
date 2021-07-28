21ea286c4275fe059b32753dfa8d6b2ca7308998
Nikita Telitsyn <dmtvn.labs@yandex.ru>
2021-07-28 15:48:02 +0300

    Added lifecycle methods and state

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,19 +1,55 @@
 import React, {Component} from 'react';
 import './itemList.css';
+import GotService from "../../services/GotService";
+import Spinner from '../spinner';
+
 export default class ItemList extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    gotService = new GotService();
+
+    state = {
+        charList: null
+    }
+
+    componentDidMount() {
+        this.gotService.getAllCharacters()
+            .then( (charList) => {
+                this.setState({
+                    charList
+                })
+            })
+    }
+
+    renderItems(arr) {
+        return arr.map((item, i) => {
+            return (
+                    <li 
+                        key={Math.floor(Math.random() * 1000)}
+                        onClick={() => this.props.onCharSelected( 41 + i)}
+                        className="list-group-item">
+                        {item.name}
+                    </li>
+            )
+        })
+    }
 
     render() {
+        
+        const {charList} = this.state;
+
+        if (!charList) {
+            return <Spinner/>
+        }
+
+        const items = this.renderItems(charList);
+
         return (
             <ul className="item-list list-group">
-                <li className="list-group-item">
-                    John Snow
-                </li>
-                <li className="list-group-item">
-                    Brandon Stark
-                </li>
-                <li className="list-group-item">
-                    Geremy
-                </li>
+                
+                {items}
             </ul>
         );
     }
